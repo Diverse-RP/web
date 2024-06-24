cd15d61874f13e36096269a982dcc36ce87b40a1
stepan_valic <valicekstepan8@seznam.cz>
2024-06-24 22:36:06 +0200

    sdf

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleSwitch = document.querySelector('.input');
+    const currentTheme = localStorage.getItem('theme') || 'light';
+
+    if (currentTheme === 'dark') {
+        document.body.classList.add('dark-theme');
+        toggleSwitch.checked = true;
+    }
+
+    toggleSwitch.addEventListener('change', function () {
+        if (toggleSwitch.checked) {
+            document.body.classList.add('dark-theme');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.body.classList.remove('dark-theme');
+            localStorage.setItem('theme', 'light');
+        }
+    });
+
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        if (link.href === window.location.href) {
+            link.classList.add('active');
+        }
+    });
+});
